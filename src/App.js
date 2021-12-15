9815db121ac0be0d50f1f159237b0d71c5d67b71
KotturMohammedTalha <mohammed.t@vuedata.in>
2021-12-15 16:40:35 +0530

    Birthday page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Birthday from './Components/birthday';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Birthday />
     </div>
   );
 }
