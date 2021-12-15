5eaa091775b2c158a23f24bae3501881b5fdfb70
KotturMohammedTalha <mohammed.t@vuedata.in>
2021-12-15 17:11:18 +0530

    Form Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Birthday from './Components/birthday';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Birthday />
+      {/* <Birthday /> */}'
+      <Form/>
     </div>
   );
 }
