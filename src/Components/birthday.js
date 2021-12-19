a3392824b6620c72d59401aa581f2b975243c040
KotturMohammedTalha <mohammed.t@vuedata.in>
2021-12-19 19:42:07 +0530

    Changes in UI

--- src/Components/birthday.js
+++ src/Components/birthday.js
@@ -1,3 +1,4 @@
+import { Grid, Paper, Stack } from "@mui/material";
 import React, { useState } from "react";
 
 function Birthday() {
@@ -5,22 +6,55 @@ function Birthday() {
     const [ageJohn, setAgeJohn] = useState(88);
     return (
       <div>
-        <div>
+        <h1>Birthday App</h1>
+         <Stack
+        direction="row"
+        spacing={3}
+        style={{ paddingBottom: "10px", paddingTop: "10px" }}
+      >
+        
+        <Grid xs={6} md={6}>
+          <Paper
+            elevation={20}
+            style={{
+              margin: "20px auto",
+              width: "300px",
+              padding: "30px 20px",
+              fontSize: "18px",
+              fontWeight: 600,
+              textAlign: "center",
+            }}
+          >
           <h1>Doe, Jane</h1>
           <h3>Age : {ageJane}</h3>
           <h3>Hair Color : Black </h3>
           <button onClick={() => setAgeJane(ageJane + 1)}>
             Birthday Button for Jane Doe
           </button>
-        </div>
-        <div>
+          </Paper>
+          </Grid>
+          
+        <Grid xs={6} md={6}>
+          <Paper
+            elevation={20}
+            style={{
+              margin: "20px auto",
+              width: "300px",
+              padding: "30px 20px",
+              fontSize: "18px",
+              fontWeight: 600,
+              textAlign: "center",
+            }}
+          >
           <h1>Smith, John</h1>
           <h3>Age : {ageJohn}</h3>
           <h3>Hair Color : Brown </h3>
           <button onClick={() => setAgeJohn(ageJohn + 1)}>
             Birthday Button for John Smith
           </button>
-        </div>
+          </Paper>
+          </Grid>
+        </Stack>
       </div>
     );
 }
