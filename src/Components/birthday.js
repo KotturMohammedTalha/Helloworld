fa17e9c7ebc3c28f82b6eb32d82d96bcec47fa15
KotturMohammedTalha <mohammed.t@vuedata.in>
2021-12-19 21:05:19 +0530

    changes in css

--- src/Components/birthday.js
+++ src/Components/birthday.js
@@ -1,4 +1,4 @@
-import { Grid, Paper, Stack } from "@mui/material";
+import { Button, Grid, Paper, Stack } from "@mui/material";
 import React, { useState } from "react";
 
 function Birthday() {
@@ -23,14 +23,15 @@ function Birthday() {
               fontSize: "18px",
               fontWeight: 600,
               textAlign: "center",
+              backgroundColor:'rgba(221, 209, 209, 0.87)'
             }}
           >
           <h1>Doe, Jane</h1>
           <h3>Age : {ageJane}</h3>
           <h3>Hair Color : Black </h3>
-          <button onClick={() => setAgeJane(ageJane + 1)}>
+          <Button variant="contained" onClick={() => setAgeJane(ageJane + 1)}>
             Birthday Button for Jane Doe
-          </button>
+          </Button>
           </Paper>
           </Grid>
           
@@ -44,14 +45,15 @@ function Birthday() {
               fontSize: "18px",
               fontWeight: 600,
               textAlign: "center",
+              backgroundColor:'rgba(221, 209, 209, 0.87)'
             }}
           >
           <h1>Smith, John</h1>
           <h3>Age : {ageJohn}</h3>
           <h3>Hair Color : Brown </h3>
-          <button onClick={() => setAgeJohn(ageJohn + 1)}>
+          <Button variant="contained" onClick={() => setAgeJohn(ageJohn + 1)}>
             Birthday Button for John Smith
-          </button>
+          </Button>
           </Paper>
           </Grid>
         </Stack>
