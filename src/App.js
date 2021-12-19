a0cb06ca2ccb55ae8d10096ebb33b0273318c90c
KotturMohammedTalha <mohammed.t@vuedata.in>
2021-12-19 19:23:00 +0530

    Cleaned components folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Birthday from './Components/birthday';
 import Form from "./Components/Form";
-import LinkCom from "./Components/LinkCom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Birthday /> */}'
       <Form/>
-      {/* <LinkCom /> */}
     </div>
   );
 }
